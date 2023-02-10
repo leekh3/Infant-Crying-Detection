f20f8d58c16ae58529eaa5975f22c9ba7de5e13e
Kyunghun Lee <jimmy@Kyunghuns-MacBook-Pro.local>
2023-02-10 13:49:39 -0500

    doc updated

--- driver_LENA_segment_into_2min.py
+++ driver_LENA_segment_into_2min.py
@@ -15,31 +15,40 @@ home = expanduser("~")
 # Find input files from input folder.
 
 # find all folders
-inFile = glob.glob(home + "/data/LENA/1198_LENA/AN1/*.wav")[0]
-outFolder = home + "/data/LENA/1198_LENA/AN1/segmented_2min/"
-def makeDirIfNotExist(path):
-# path = "pythonprog"
-# Check whether the specified path exists or not
-    isExist = os.path.exists(path)
-    if not isExist:
-       # Create a new directory because it does not exist
-       os.makedirs(path)
-       print("The new directory is created!")
-makeDirIfNotExist(outFolder)
+inFiles = glob.glob(home + "/data/LENA/*/AN1/*.wav")
+inFiles.sort()
+outFolders = glob.glob(home + "/data/LENA/*/AN1/")
+for i in range(len(outFolders)):
+    outFolders[i] = outFolders[i] + 'segmented_2min/'
+outFolders.sort()
 
-# Read wav file.
-sound = AudioSegment.from_wav(inFile)
-wavLen = 2 * 60 # 2 min ==> 2*60 sec.
+for i,inFile in enumerate(inFiles):
+    # inFile = glob.glob(home + "/data/LENA/1198_LENA/AN1/*.wav")[0]
+    # outFolder = home + "/data/LENA/1198_LENA/AN1/segmented_2min/"
+    outFolder = outFolders[i]
+    def makeDirIfNotExist(path):
+    # path = "pythonprog"
+    # Check whether the specified path exists or not
+        isExist = os.path.exists(path)
+        if not isExist:
+           # Create a new directory because it does not exist
+           os.makedirs(path)
+           print("The new directory is created!")
+    makeDirIfNotExist(outFolder)
 
-t1,t2 = 0,wavLen*1000
-fileIdx = 0
-while(t2<len(sound)):
-    newAudio = sound[t1:t2]
-    newAudio.export(outFolder + '/' + str(fileIdx)+'.wav', format="wav")
-    t1 += wavLen*1000
-    t2 += wavLen*1000
-    fileIdx += 1
-if t1<t2:
-    newAudio = sound[t1:]
-    newAudio.export(outFolder + '/' + str(fileIdx) + '.wav', format="wav")
+    # Read wav file.
+    sound = AudioSegment.from_wav(inFile)
+    wavLen = 2 * 60 # 2 min ==> 2*60 sec.
+
+    t1,t2 = 0,wavLen*1000
+    fileIdx = 0
+    while(t2<len(sound)):
+        newAudio = sound[t1:t2]
+        newAudio.export(outFolder + '/' + str(fileIdx)+'.wav', format="wav")
+        t1 += wavLen*1000
+        t2 += wavLen*1000
+        fileIdx += 1
+    if t1<t2:
+        newAudio = sound[t1:]
+        newAudio.export(outFolder + '/' + str(fileIdx) + '.wav', format="wav")
 

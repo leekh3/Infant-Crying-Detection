8bdaab6982dd68f4271d92d96fdfcc324fa4d4f4
Kyunghun Lee <Kyunghun.lee@nih.gov>
2023-04-05 15:16:36 -0400

    doc updated

--- driver_read_elan_label_processing_03302023.py
+++ driver_read_elan_label_processing_03302023.py
@@ -14,26 +14,8 @@ import glob
 import pandas as pd
 import os
 
-# Read the CSV file into a pandas DataFrame object
-# df = pd.read_csv('filename.csv', header=None)
-
-# # Iterate through each row in the DataFrame
-# for index, row in df.iterrows():
-#     # Access the columns in each row using index numbers
-#     column1 = row[0]
-#     column2 = row[1]
-#     # ...and so on for each column
-#     print(column1, column2)
-
+# Part1: Read generated lables (ELAN)
 home = expanduser("~")
-# inFiles = glob.glob(home + "/data/ELAN_generated_label/ELAN_generated_label_03302023/*/*.txt")
-# inFiles.sort()
-# for i,inFile in enumerate(inFiles):
-#     if os.path.getsize(inFile) < 0:
-#         continue
-    # df = pd.read_csv(inFile, header=None)
-    # df = pd.read_csv(inFile, delimiter=',| |\t', engine='python',header=None)
-
 inFiles = glob.glob(home + "/data/ELAN_generated_label/ELAN_generated_label_04052023/*/*.txt")
 outFile = home + "/data/ELAN_generated_label/ELAN_generated_label_04052023/summary.csv"
 inFiles.sort()
@@ -55,22 +37,115 @@ for i,inFile in enumerate(inFiles):
     df = pd.concat([df,df_tmp])
 
 df.columns = headers
-
-# df['label'] = df['s'].str.replace('S')
-
-
-
 df = df.drop('label', axis=1)
 print(df)
 
-
-
 # save DataFrame as a CSV file
-df.to_csv(outFile, index=False)
+# df.to_csv(outFile, index=False)
 
-# Check unique label
-label = df['label']
-label.dropna()
-unique_values = label.unique()
-print(unique_values)
->>>>>>> c5af09d (doc updated)
+# Save only crying dataset.
+df = df.loc[df['Type'] == 'Cry [Cr]']
+
+# Find subject number from path and find corresponding extracted 10 min wav files.
+SDANs= []
+pathList = list(df['path'])
+wavFiles = glob.glob(home + "/data/LENA/random_10min_extracted_04052023/*.wav")
+wavList = []
+for path in pathList:
+    SDAN = path.split('/')[0]
+    SDANs.append(SDAN)
+    for index, wavFile in enumerate(wavFiles):
+        wavFile = wavFile.split('/')[-1]
+        if SDAN in wavFile:
+            wavList.append(wavFiles[index])
+            break
+
+# Part2: Divide 10 min WAV files into 5x2 min wav files.
+df['SDAN'] = SDANs
+df['WAV'] = wavList
+wavSet = set(wavList)
+wavFolder_2min = home + "/data/LENA/random_10min_extracted_04052023/segmented_2min/"
+
+if not os.path.exists(wavFolder_2min):
+    os.makedirs(wavFolder_2min)
+    print(f"Directory '{wavFolder_2min}' created.")
+else:
+    print(f"Directory '{wavFolder_2min}' already exists.")
+
+# Generate 2 min wav files from 10 min wav files.
+import librosa
+import re
+import soundfile as sf
+for index,input_wav in enumerate(wavSet):
+    # Find SDAN from input wav name
+    match = re.search(r'\d+', input_wav.split('/')[-1])
+    sdan = ''
+    if match:
+        sdan = match.group(0)
+    else:
+        print(f"No number found.")
+        break
+    output_folder = wavFolder_2min + sdan
+
+    # create the output folder if it does not exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # load the input WAV file
+    y, sr = librosa.load(input_wav, sr=None)
+
+    # set the duration of each segment to 2 minutes
+    segment_duration = 2 * 60
+
+    # set the number of samples per segment
+    segment_samples = segment_duration * sr
+
+    # iterate over the 5 segments
+    for i in range(5):
+        # set the start and end samples for the current segment
+        start_sample = i * segment_samples
+        end_sample = (i + 1) * segment_samples
+
+        # set the output file path
+        output_file = os.path.join(output_folder, f'{i}.wav')
+
+        # save the current segment to the output file
+        sf.write(output_file, y[start_sample:end_sample], sr, format='WAV', subtype='PCM_16')
+
+# Part3: Run Yao's preprocessing/prediction script (refer: previous my code: driver_baseline_concatenate_deBarbaroCry_2min.py)
+from preprocessing import preprocessing
+from predict import predict
+
+inFolders = []
+for sdan in set(SDANs):
+    inFolders.append(home + "/data/LENA/random_10min_extracted_04052023/segmented_2min/" + sdan)
+
+for inFolder in inFolders:
+    preprocessedFolder = inFolder + '/preprocessed/'
+    predictedFolder = inFolder + '/predicted/'
+    # create the output folder if it does not exist
+    if not os.path.exists(preprocessedFolder):
+        os.makedirs(preprocessedFolder)
+    if not os.path.exists(predictedFolder):
+        os.makedirs(predictedFolder)
+
+    inFiles = glob.glob(inFolder + '/*.wav')
+    for inFile in inFiles:
+        preprocessedFile = preprocessedFolder + re.findall(r'\d+', inFile.split('/')[-1])[0] + '.csv'
+        predictedFile = predictedFolder + re.findall(r'\d+', inFile.split('/')[-1])[0] + '.csv'
+
+        # Run Preproecessing
+        print(inFile)
+        print(preprocessedFile)
+        preprocessing(inFile, preprocessedFile)
+
+        # Run Prediction script
+        predict(inFile, preprocessedFile, predictedFile)
+
+
+# # Check unique label
+# label = df['label']
+# label.dropna()
+# unique_values = label.unique()
+# print(unique_values)
+# >>>>>>> c5af09d (doc updated)
